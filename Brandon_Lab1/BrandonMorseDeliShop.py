24c2fa8d1d2811fe1e956b4d220fd76f50e0c95b
Brandon Morse <Brandonmorseaz@gmail.com>
2026-08-08 07:32:04 -0400

    Added sandwich choice loop

--- Brandon_Lab1/BrandonMorseDeliShop.py
+++ Brandon_Lab1/BrandonMorseDeliShop.py
@@ -2,7 +2,7 @@
 
 # Step 1: Define the sandwiches and their prices for the deli as a list/array
 sandwiches = {
-    1: ("Turkey Bacon Sanwich", 5.59),
+    1: ("Brandon's Club Sanwich", 5.59),
     2: ("Ham Sandwich", 4.99),
     3: ("Veggie Sandwich", 6.49),
     4: ("Grilled Chicken Sandwich", 7.99)
@@ -19,5 +19,17 @@ def show_menu():
             # display the total with 2 decimal points (like normal money is displayed)
             print(f"{key}. {name} - ${price:.2f}")   
 
-# Step 4:              
+# Step 4: Ask the user for a number to choose a sandwich
+def get_sandwich_choice():
+    # Only continue if the user enters a valid number
+    while True:
+        try:
+            choice = int(input("Choose a sandwich(number): "))
+            if choice in sandwiches:
+                 return sandwiches[choice]
+            # Do not continue unless the user inputs a number, and that number must be between 1-4 for the listed options
+            else:
+                 print ("Invalid choice. Please Enter a number 1-4")
+        except ValueError:
+             print("Please Enter a NUMBER value between 1-4")
 
