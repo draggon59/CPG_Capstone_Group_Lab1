d36474d3f948fdde9ae2da5476524e0dfcb90b27
Brandon Morse <Brandonmorseaz@gmail.com>
2026-08-08 07:23:15 -0400

    added first 3 steps to the deli

---
+++ Brandon_Lab1/BrandonMorseDeliShop.py
@@ -0,0 +1,23 @@
+# Brandon Morse Simple Deli Shop #
+
+# Step 1: Define the sandwiches and their prices for the deli as a list/array
+sandwiches = {
+    1: ("Turkey Bacon Sanwich", 5.59),
+    2: ("Ham Sandwich", 4.99),
+    3: ("Veggie Sandwich", 6.49),
+    4: ("Grilled Chicken Sandwich", 7.99)
+}
+
+# Step 2: Define the chips and drinks along with their prices (simple for now and not an entire list like the sandwiches)
+chips_price = 2.99
+drink_price = 2.49
+
+# Step 3: Logic to Display the menu
+def show_menu():
+        print ("\n---Brandon Morse Deli---")
+        for key, (name, price) in sandwiches.items():
+            # display the total with 2 decimal points (like normal money is displayed)
+            print(f"{key}. {name} - ${price:.2f}")   
+
+# Step 4:              
+
