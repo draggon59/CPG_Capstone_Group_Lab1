7557beb2e341c55cf877e34a65e5cf1bf1f3c0fe
Brandon Morse <Brandonmorseaz@gmail.com>
2026-08-09 07:08:41 -0400

    Added step 3, which prorates expenses based on the current date to handle times when the user is not calculating at the first of the month

--- Brandon_Lab1/BrandonMorseSavingsCalculator.py
+++ Brandon_Lab1/BrandonMorseSavingsCalculator.py
@@ -35,4 +35,58 @@ def count_paychecks(start_date, end_date, first_payday, pay_frequency):
     return count  
 
 
-# Step 3: 
+# Step 3: Calculate the number of days for a full month of expenses to subtract from the total
+
+def calculate_expenses(monthly_expenses, start_date, end_date):
+
+    current_date = start_date
+    total_expenses = Decimal("0")
+
+    while current_date <= end_date:
+
+        month_start = current_date.replace(day = 1)
+
+        if current_date.month == 12:
+            next_month = current_date.replace(
+                year = current_date.year + 1,
+                month = 1,
+                day = 1
+            )
+        else: next_month = current_date.replace(
+            month = current_date.month + 1,
+            day = 1
+        )  
+
+        days_in_month = (next_month - month_start).days
+
+        month_end = min(
+            end_date,
+            next_month - timedelta(days = 1)
+        )    
+
+        applicable_days = (month_end - current_date).days + 1
+
+        monthly_portion = (
+            monthly_expenses * 
+            Decimal(applicable_days) /
+            Decimal(days_in_month)
+        )
+
+        total_expenses += monthly_portion
+
+        current_date = next_month
+
+        return money(total_expenses)
+
+def calculate_finances(
+        paycheck_amount,
+        pay_frequency,
+        first_payday,
+        monthly_expenses,
+        end_date
+):
+
+    today = date.today()
+
+
+    
